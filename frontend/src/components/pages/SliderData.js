58d6f233103d2d5ddf10442c07c660138d45bc41
oscarbravo96 <96oscarbravo@gmail.com>
2022-03-30 19:10:20 -0400

    added carousel sort of

---
+++ frontend/src/components/pages/SliderData.js
@@ -0,0 +1,15 @@
+export const SliderData = [
+    {
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Typical_Tetris_Game.svg/800px-Typical_Tetris_Game.svg.png'
+    },
+    {
+      image:
+        'https://1.bp.blogspot.com/-VoKIrzdjBuk/Xn1q3LhNToI/AAAAAAAAW64/O7jRIrhA7rYrOy3iDQMkbSxEOiFw9bcOACLcBGAsYHQ/s1600/space-invaders.jpg'
+    },
+    {
+      image:
+        'https://opengameart.org/sites/default/files/preview_937.png'
+    },
+    
+  ];
