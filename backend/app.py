321c79119dcf08e07baaf81938447df76d7c6f60
oscarbravo96 <96oscarbravo@gmail.com>
2022-04-01 22:15:07 -0400

    hash

--- backend/app.py
+++ backend/app.py
@@ -18,7 +18,7 @@ bcrypt = Bcrypt(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True, nullable=False)
-    password = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
     def __init__(self, username, password):
         self.username = username
